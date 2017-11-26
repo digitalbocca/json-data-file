41f3b474769137c7194e0aad1b4253ccf9830168
Gabriel Bertola Bocca <digitalbocca@gmail.com>
2017-11-26 18:07:33 -0200

    Leitura e Gravação do arquivo log.json

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 ## v0.1.0 ##
 
 > Ler e Gravar dados em um arquivo JSON.
+
+## CHANGELOG ##
+
+**Mudanças na v0.2.0**
+
+- Leitura e Gravação do arquivo log.json.
+- Seguindo standardjs.
--- index.js
+++ index.js
@@ -1,1 +1,27 @@
+'use strict'
+
+const fs = require('fs')
+const moment = require('moment')
+const arquivo = fs.readFileSync('./log.json', 'utf8')
+const parser = JSON.parse(arquivo)
+
+console.log('Lendo o arquivo...')
+
+moment.locale('pt-br')
+const chave = moment()
+const valor = moment(chave).format('LLLL')
+
+console.log('Gerando novo valor: ', valor)
+
+parser.logs[chave] = valor
+
+fs.unlinkSync('./log.json')
+
+console.log('Apagando Arquivo Antigo.')
+
+const stringToFile = JSON.stringify(parser)
+
+fs.writeFileSync('./log.json', stringToFile, 'utf8')
+
+console.log('Gerado novo arquivo de Log.')
 console.log('(c)2017 - Estúdio Digital Bocca')
